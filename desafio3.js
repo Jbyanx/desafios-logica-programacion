4fa81dc5896fb591c6c11bd558b7e85f8b2f3a79
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-06-20 17:29:48 -0500

    Create desafio3.js

---
+++ desafio3.js
@@ -0,0 +1,31 @@
+//contador de 1 a 10
+let contador = 1;
+
+while(contador <= 10 ){
+    console.log(contador+"\n");
+    contador++;
+}
+
+//contador de 10 a 0
+let contador2 = 10;
+
+while(contador2 >= 0 ){
+    console.log(contador2+"\n");
+    contador2--;
+}
+
+//cuenta regresiva
+let numero = parseInt(prompt("Ingrese un numero"));
+
+while(numero >= 0){
+    console.log(numero+"\n");
+    numero--;
+}
+
+//cuenta progresiva
+let numero2 = parseInt(prompt("Ingrese un numero"));
+
+while(numero2 <= 10){
+    console.log(numero2+"\n");
+    numero2++;
+}
