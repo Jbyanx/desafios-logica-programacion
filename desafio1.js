f35a7c30fd513cecfedbddbc7794bfc50815931e
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-06-20 16:34:49 -0500

    Create desafio1.js

---
+++ desafio1.js
@@ -0,0 +1,17 @@
+alert("¡Bienvenida y bienvenido a nuestro sitio web!");
+
+let nombre = "luna";
+let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+
+alert("¡Error! Completa todos los campos");
+
+let mensajeDeError = "¡Error! Completa todos los campos";
+
+nombre = prompt("Escribe tu nombre");
+edad = prompt("Escribe tu edad");
+
+if(edad >= 18 ){
+    alert("¡Puedes obtener tu licencia de conducir!");
+}
